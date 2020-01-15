954fb967c86d41dbb6e4de57c13077d8e7881d8d
Jouko Rintamäki <jouko.rintamaki@live.fi>
2020-01-15 22:13:01 +0200

    .

--- 3.08a.sh
+++ 3.08a.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
+set -ex
+
 #install Azure Cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash	
 #login to azure cloud service
-az login
+az login --output none
 #set azure subscription
-az account set --subscription 90770ece-195c-4c66-b939-887b721beaa1
+az account set --subscription b9b3fbea-71e8-49dd-9aab-0bae7424d14c --output none
 #variables
 PROJECT=devopswithdocker2019
 LOCATION=westeurope
@@ -17,16 +18,17 @@ AKSVERSION=$(az aks get-versions -l $LOCATION --query 'orchestrators[-1].orchest
 az group create --name $RESOURCE_GROUP --location $LOCATION
 #create azure kuberneter service
 az aks create --resource-group $RESOURCE_GROUP --name $AKS_NAME --enable-addons monitoring --kubernetes-version $AKSVERSION --generate-ssh-keys --location $LOCATION
+#install k8s cli
+az aks install-cli
+#connect aks
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_NAME
+#list nodes
 kubectl get nodes
 
+#variables
 REGISTER=joukorintamaki
 EXERCISE=3.08a
 mkdir -p $EXERCISE
-cat >$EXERCISE/docker-compose.yml <<EOF
-version: "3"
-services:
-EOF
 
 TAG=backend-example-docker
 FOLDER=$EXERCISE/$TAG
@@ -45,19 +47,8 @@ WORKDIR /usr/src/$TAG
 EXPOSE $PORT
 CMD ["npm","start"]
 EOF
-cat >>$EXERCISE/docker-compose.yml <<EOF
- $TAG:
-  build: ./$TAG
-  image: $REGISTER/$TAG
-  container_name: $TAG
-  environment:
-   - REDIS=redis
-   - DB_USERNAME=postgres
-   - DB_PASSWORD=salasana
-   - DB_HOST=postgres
-  ports:
-   - $PORT:$PORT
-EOF
+docker build --tag $REGISTER/$TAG $FOLDER
+docker push $REGISTER/$TAG
 
 TAG=frontend-example-docker
 FOLDER=$EXERCISE/$TAG
@@ -76,82 +67,90 @@ WORKDIR /usr/src/$TAG
 EXPOSE $PORT
 CMD ["npm","start"]
 EOF
-cat >>$EXERCISE/docker-compose.yml <<EOF
- $TAG:
-  build: ./$TAG
-  image: $REGISTER/$TAG
-  container_name: $TAG
+docker build --tag $REGISTER/$TAG $FOLDER
+docker push $REGISTER/$TAG
+
+cat >$EXERCISE/deploy.yaml <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: frontend-example-docker
+  name: frontend-example-docker
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: frontend-example-docker
+  template:
+    metadata:
+      labels:
+        app: frontend-example-docker
+    spec:
+      containers:
+        - image: joukorintamaki/frontend-example-docker
+          name: frontend-example-docker
+          ports:
+            - containerPort: 5000
+      nodeSelector:
+        kubernetes.io/hostname: aks-nodepool1-16828242-vmss000000
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: backend-example-docker
+  name: backend-example-docker
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: backend-example-docker
+  template:
+    metadata:
+      labels:
+        app: backend-example-docker
+    spec:
+      containers:
+        - image: joukorintamaki/backend-example-docker
+          name: backend-example-docker
+          ports:
+            - containerPort: 8000
+      nodeSelector:
+        kubernetes.io/hostname: aks-nodepool1-16828242-vmss000001
+---
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    app: frontend-example-docker
+  name: frontend-example-docker
+spec:
   ports:
-   - $PORT:$PORT
-EOF
-
-TAG=redis
-cat >>$EXERCISE/docker-compose.yml <<EOF
- $TAG:
-  container_name: $TAG
-  image: $TAG:alpine
+    - port: 80
+      protocol: TCP
+      targetPort: 5000
+  selector:
+    app: frontend-example-docker
+  type: LoadBalancer
+---
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    app: backend-example-docker
+  name: backend-example-docker
+spec:
   ports:
-    - 6379
-EOF
-
-TAG=postgres
-cat >>$EXERCISE/docker-compose.yml <<EOF
- $TAG:
-  container_name: $TAG
-  image: $TAG:alpine  
-  environment:
-   - POSTGRES_USERNAME=postgres
-   - POSTGRES_PASSWORD=salasana
-  volumes:
-   - database:/var/lib/postgresql/data
-EOF
-
-TAG=nginxservice
-FOLDER=$EXERCISE/$TAG
-mkdir -p $FOLDER
-cat >$FOLDER/nginx.conf <<EOF
-events { worker_connections 1024; }
-http {
- server {
-  listen 80;
-
-  location / {
-   proxy_pass http://frontend-example-docker:5000/;
-  }
-
-  location /api/ {
-   proxy_pass http://backend-example-docker:8000/;
-  }
- }
-}
-EOF
-cat >$FOLDER/Dockerfile <<EOF
-FROM nginx:alpine
-COPY nginx.conf /etc/nginx/nginx.conf
-EOF
-cat >>$EXERCISE/docker-compose.yml <<EOF
- $TAG:  
-  build: ./$TAG
-  image: $REGISTER/$TAG  
-  container_name: $TAG
-  ports:
-   - 80:80
-  environment:
-   - NGINX_PORT=80
-EOF
-
-cat >>$EXERCISE/docker-compose.yml <<EOF
-volumes:
- database:
+    - port: 8000
+      protocol: TCP
+      targetPort: 8000
+  selector:
+    app: backend-example-docker
+  type: LoadBalancer
 EOF
 
 cd $EXERCISE
-
-docker-compose build --parallel
-docker-compose push
-docker image ls
-
-echo "docker stack deploy --orchestrator=kubernetes --compose-file docker-compose.yml $PROJECT"
-
-#docker image remove frontend-example-docker backend-example-docker
-#docker-compose up -d
+kubectl apply -f deploy.yaml
+sleep 10m
+kubectl get deployments,pods,services,endpoints -o wide
